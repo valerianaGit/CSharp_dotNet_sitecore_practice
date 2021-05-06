a16f449c21245e2d56b7dc4feed488a299568f0a
VRuiz <valeria.do@gmail.com>
2021-05-06 15:53:16 -0500

    create syntax

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -8,6 +8,11 @@ namespace myApp
         {
             Console.WriteLine("Hello World!");
                      Console.WriteLine("The current time is " + DateTime.Now);
+                     if (args.Length > 0) {
+Console.WriteLine($"Hello, {args[0]}! My name is C#");
+                     } else {
+                         Console.WriteLine("Hello, I am an else statement");
+                     }
         }
     }
 }
--- myApp/test/GradeBook.Tests/UnitTest1.cs
+++ myApp/test/GradeBook.Tests/UnitTest1.cs
@@ -1,11 +1,11 @@
 using System;
-using Xunit;
+//using Xunit;
 
 namespace GradeBook.Tests
 {
     public class UnitTest1
     {
-        [Fact]
+        //[Fact]
         public void Test1()
         {
 
