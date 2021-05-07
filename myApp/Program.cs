8e24c3372cc2e9c68d1321df148751cf3b018520
VRuiz <valeria.do@gmail.com>
2021-05-07 11:26:50 -0500

    for loop

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -21,13 +21,20 @@ numbers[2] = 14.5;
 
 //Array implementation
  double[] numbersa = new double[] {12.7, 13.4, 14.5, 8.9};
-
 //or 
 var numbersb = new[] {2.7, 13.4, 14.5, 8.9};
 // Sum all items -> naive approach
 var result  = numbers[0];
-result = result + numbers[1];
+result += numbers[1];
 result = result + numbers[2];
+//for loop 
+var resultForeach = 0.0;
+foreach(double number in numbers) {
+resultForeach += number;
+
+}
+
+            Console.WriteLine($"Summations of both float numbers is {summationOfFloats}");
                      Console.WriteLine("The current time is " + DateTime.Now);
                      if (args.Length > 0) {
 Console.WriteLine($"Hello, {args[0]}! My name is C#");
