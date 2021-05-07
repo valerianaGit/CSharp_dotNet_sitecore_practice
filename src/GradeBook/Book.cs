b099eadb6186c510745da341a0b5b0c2294ef9dc
VRuiz <valeria.do@gmail.com>
2021-05-07 12:56:44 -0500

    switch over code into uppe rlevel GradeBok, to eb able to run and debug code as i go

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-namespace myApp{
+namespace GradeBook{
 class Book {
 static void methods() {
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello valeria!");
+//CLASSES AND OBJECTS 
+
+var book = new Book();
+book.addGrade(20.5);
+
+
+
+
+
+
+
+
+
+            // SYNTAX!
+            double x = 34.12;
+            // add 2 floats and print the result 
+            float float1 = 35.25f;
+            float float2 = 3.45F;
+            float summationOfFloats = float1 + float2;
+//Arrays
+        //    Create => 
+        double[] numbers = new double[3];
+//Access => 
+double first = numbers[0] = 12.7;
+numbers[1] = 13.4;
+numbers[2] = 14.5;
+
+//Array implementation
+ double[] numbersa = new double[] {12.7, 13.4, 14.5, 8.9};
+//or 
+var numbersb = new[] {2.7, 13.4, 14.5, 8.9};
+// Sum all items -> naive approach
+var result  = numbers[0];
+result += numbers[1];
+result = result + numbers[2];
+//for loop 
+var resultF = 0.0;
+foreach(double number in numbersb) {
+resultF += number;
+}
+resultF /= numbersb.Length;
+Console.WriteLine($" average grade for numbersb list: {resultF:N3}");
+
+//COLLECTIONS
+//LIST -> Mutable array 
+List<double> grades = new List<double>(){12.7, 13.4, 14.5, 8.9}; //initializer is the curly braces {}
+grades.Add(20.9); // add value to list 
+
+            Console.WriteLine($"Summations of both float numbers is {summationOfFloats}");
+                     Console.WriteLine("The current time is " + DateTime.Now);
+                     if (args.Length > 0) {
+Console.WriteLine($"Hello, {args[0]}! My name is C#");
+                     } else {
+                         Console.WriteLine("Hello, I am an else statement");
+                     }
         }
     }
 }
