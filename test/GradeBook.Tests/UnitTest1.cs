4f34a9b47156cf628bd93142edfd8eb6e0a7429d
VRuiz <valeria.do@gmail.com>
2021-05-07 17:10:43 -0500

    Create a test ARRANGE -> ACT -> ASSERT

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -5,10 +5,17 @@ namespace GradeBook.Tests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Fact] // attribute , data attached to symbol that follows it i.e. Test1
         public void Test1()
         {
-
+            //arrange
+            var x = 5;
+            var y = 2; 
+            var expected = 7;
+            //Act 
+            var actual = x * y;
+            //Assert
+Assert.Equal(expected, actual);
         }
     }
 }
